64011aa249e4cd8f566ac72987187a89fc2a2a8c
Valeri Tandilash <tandilashvilivaleri@gmail.com>
2020-12-09 18:49:52 +0400

    A Very Big Sum

---
+++ warmup/a-very-big-sum.php
@@ -0,0 +1,11 @@
+<?php
+
+// URL: https://www.hackerrank.com/challenges/a-very-big-sum
+
+function aVeryBigSum($ar) {
+    // calculates the sum of the array elements
+    return array_sum($ar);
+}
+
+
+echo aVeryBigSum([7, 8, 15]);
