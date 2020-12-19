8ba2d63d21bd6957369326b0145c162ec4b2b930
Valeri Tandilash <tandilashvilivaleri@gmail.com>
2020-12-19 16:52:51 +0400

    Added David Bagaturia's solution to 'utopian tree' problem

--- implementation/utopian-tree.php
+++ implementation/utopian-tree.php
@@ -42,7 +42,6 @@ function utopianTree($n) {
 
 echo utopianTree(5);
 
-
 /*  Patterns for creating Math formula
 0      1    2** (0/2 + 1) - 1 = 2**1-1 = 1
 1      2    (2** (1/2 + 1) - 1) * 2 = 2
@@ -53,4 +52,33 @@ echo utopianTree(5);
 6	   15   2** (6/2 + 1) - 1 = 2**4-1 = 15
 7 	   30   (2** (7/2 + 1) - 1) * 2 = 30
 8 	   31   ...
+*/
+
+
+
+
+/*
+ციკლი 	ხის სიმაღლე	ორობითი სისტემა 
+0	1	1
+1	2	10
+2	3	11
+3	6	110
+4	7	111
+5	14	1110
+
+// David Bagaturia's solution
+int utopianTree(int n) {
+
+    if (n == 0)
+        return 1;
+    else
+        n--;
+
+    unsigned long long int H = 18446744073709551615;
+    int shiftLeft = 63 - int(n / 2);
+    int value = n % 2;
+
+    H = ((H >> shiftLeft) << 1) + value;
+    return H;
+}
 */
